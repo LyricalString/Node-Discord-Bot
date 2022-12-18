7186dcb9a9557d5285352d821daa12b26f7d36cf
eliyya <tecno_eli@hotmail.com>
2022-12-18 01:25:50 -0600

    remove unesesary discord-menus

--- commands/Información y Utilidades/lang.js
+++ commands/Información y Utilidades/lang.js
@@ -1,195 +1,108 @@
 const { MessageEmbed } = require('discord.js')
+const { SelectMenuBuilder, ActionRowBuilder } = require('@discordjs/builders')
 const Command = require('../../structures/Commandos.js')
 const userModel = require('../../models/user.js')
-const { DiscordMenus, MenuBuilder } = require('discord-menus')
 
 module.exports = class Lang extends Command {
     constructor(client) {
         super(client, {
             name: 'lang',
-            description: [
-                'Select your language using this menu.',
-                'Seleccione su idioma usando este menú.'
-            ],
+            description: ['Select your language using this menu.', 'Seleccione su idioma usando este menú.'],
             alias: ['language', 'idioma', 'lenguaje'],
             cooldown: 5,
             category: 'info'
         })
     }
+    /**
+     *
+     * @param {import('discord.js').Client} client
+     * @param {import('discord.js').Message} message
+     * @param {string[]} args
+     * @param {string} prefix
+     * @param {string} lang
+     * @param {*} webhookClient
+     * @param {*} ipc
+     */
     async run(client, message, args, prefix, lang, webhookClient, ipc) {
         try {
-            const MenusManager = new DiscordMenus(client)
-            const myCoolMenu = new MenuBuilder()
-                .addLabel('Español', {
-                    description:
-                        'Selecciona el español como el idioma predefinido.',
-                    value: 'es',
-                    emoji: {
-                        name: '🇪🇸'
-                    }
-                })
-                .addLabel('English', {
-                    description: 'Selects english as the main language.',
-                    value: 'en',
-                    emoji: {
-                        name: '🇺🇸'
-                    }
-                })
+            const menu = new SelectMenuBuilder()
+                .setCustomId('menu1')
+                .setPlaceholder(client.language.LANGMENU[1])
                 .setMaxValues(1)
-                .setCustomID('menu1')
-                .setPlaceHolder(client.language.LANGMENU[1])
-
-            const embed = new MessageEmbed()
-                .setFooter(
-                    'Selecciona un lenguaje de la lista.',
-                    message.author.displayAvatarURL()
+                .setMinValues(1)
+                .addOptions(
+                    {
+                        label: 'Español',
+                        description: 'Selecciona el español como el idioma predefinido.',
+                        value: 'es',
+                        emoji: {
+                            name: '🇪🇸'
+                        }
+                    },
+                    {
+                        label: 'English',
+                        description: 'Selects english as the main language.',
+                        value: 'en',
+                        emoji: {
+                            name: '🇺🇸'
+                        }
+                    }
                 )
+            const embed = new MessageEmbed()
+                .setFooter('Selecciona un lenguaje de la lista.', message.author.displayAvatarURL())
                 .setColor(process.env.EMBED_COLOR)
-
-            await MenusManager.sendMenu(message, embed, { menu: myCoolMenu })
-
-            MenusManager.once('MENU_CLICKED', menu => {
-                let user = message.member.user
-                if (menu.member.id != message.author.id) {
-                    const errorembed = new MessageEmbed()
-                        .setColor('RED')
-                        .setTitle(client.language.ERROREMBED)
-                        .setDescription(client.language.LANGMENU[3])
-                        .setFooter(
-                            message.author.username,
-                            message.author.avatarURL()
-                        )
-                    return message.channel.send({ embeds: [errorembed] })
-                }
-                if (menu.values.length > 1) {
-                    const errorembed = new MessageEmbed()
-                        .setColor('RED')
-                        .setTitle(client.language.ERROREMBED)
-                        .setDescription(client.language.LANGMENU[2])
-                        .setFooter(
-                            message.author.username,
-                            message.author.avatarURL()
-                        )
-                    return message.channel.send({ embeds: [errorembed] })
-                }
-                if (menu.values[0].toLowerCase() == 'es') {
-                    try {
-                        userModel
-                            .findOne({
-                                USERID: message.author.id.toString()
-                            })
-                            .then((s, err) => {
-                                if (err) return console.error(err)
-                                message.member.user.LANG = 'es_ES'
-                                if (s) {
-                                    s.LANG = 'es_ES'
-                                    s.save().catch(err => s.update())
-                                    const embed = new MessageEmbed()
-                                        .setColor(process.env.EMBED_COLOR)
-                                        .setTitle(client.language.SUCCESSEMBED)
-                                        .setDescription(
-                                            'Has seleccionado español como tu nuevo idioma.'
-                                        )
-                                        .setFooter(
-                                            message.author.username,
-                                            message.author.avatarURL()
-                                        )
-                                    return menu.edit(embed)
-                                } else {
-                                    const user2 = new userModel({
-                                        USERID: user.id,
-                                        COMMANDS_EXECUTED: parseInt(
-                                            user.COMMANDS_EXECUTED
-                                        ),
-                                        VOTED: user.VOTED,
-                                        BANNED: user.BANNED,
-                                        LANG: 'es_ES',
-                                        DEV: user.DEV,
-                                        PREMIUM_COMMANDS: user.PREMIUM_COMMANDS,
-                                        isINDB: user.isINDB,
-                                        Roles: user.ROLES,
-                                        OLDMODE: user.OLDMODE
-                                    })
-                                    user2
-                                        .save()
-                                        .catch(err => console.error(err))
-                                    const embed = new MessageEmbed()
-                                        .setColor(process.env.EMBED_COLOR)
-                                        .setTitle(client.language.SUCCESSEMBED)
-                                        .setDescription(
-                                            'Has seleccionado español como tu nuevo idioma.'
-                                        )
-                                        .setFooter(
-                                            message.author.username,
-                                            message.author.avatarURL()
-                                        )
-                                    return menu.edit(embed)
-                                }
-                            })
-                        return
-                    } catch (error) {
-                        console.error(error)
-                    }
-                } else if (menu.values[0].toLowerCase() == 'en') {
-                    try {
-                        userModel
-                            .findOne({
-                                USERID: message.author.id.toString()
-                            })
-                            .then((s, err) => {
-                                if (err) return console.error(err)
-                                message.member.user.LANG = 'en_US'
-                                if (s) {
-                                    s.LANG = 'en_US'
-                                    s.save().catch(err => s.update())
-                                    const embed = new MessageEmbed()
-                                        .setColor(process.env.EMBED_COLOR)
-                                        .setTitle(client.language.SUCCESSEMBED)
-                                        .setDescription(
-                                            "You've selected English as your new language"
-                                        )
-                                        .setFooter(
-                                            message.author.username,
-                                            message.author.avatarURL()
-                                        )
-                                    return menu.edit(embed)
-                                } else {
-                                    const user2 = new userModel({
-                                        USERID: user.id,
-                                        COMMANDS_EXECUTED: parseInt(
-                                            user.COMMANDS_EXECUTED
-                                        ),
-                                        VOTED: user.VOTED,
-                                        BANNED: user.BANNED,
-                                        LANG: 'en_US',
-                                        DEV: user.DEV,
-                                        PREMIUM_COMMANDS: user.PREMIUM_COMMANDS,
-                                        isINDB: user.isINDB,
-                                        Roles: user.ROLES,
-                                        OLDMODE: user.OLDMODE
-                                    })
-                                    user2
-                                        .save()
-                                        .catch(err => console.error(err))
-                                    const embed = new MessageEmbed()
-                                        .setColor(process.env.EMBED_COLOR)
-                                        .setTitle(client.language.SUCCESSEMBED)
-                                        .setDescription(
-                                            "You've selected English as your new language"
-                                        )
-                                        .setFooter(
-                                            message.author.username,
-                                            message.author.avatarURL()
-                                        )
-                                    return menu.edit(embed)
-                                }
-                            })
-                        return
-                    } catch (error) {
-                        console.error(error)
-                    }
-                }
+            const m = await message.reply({
+                embeds: [embed],
+                components: [new ActionRowBuilder().setComponents(menu)]
             })
+            m.createMessageComponentCollector({ filter: (i) => i.customId === 'menu1', max: 1 }).on(
+                'collect',
+                async (interaction) => {
+                    try {
+                        let user = message.member.user
+                        if (interaction.member.id != message.author.id) {
+                            const errorembed = new MessageEmbed()
+                                .setColor('RED')
+                                .setTitle(client.language.ERROREMBED)
+                                .setDescription(client.language.LANGMENU[3])
+                                .setFooter(message.author.username, message.author.avatarURL())
+                            return message.channel.send({ embeds: [errorembed] })
+                        }
+                        const lang = interaction.values[0] === 'es' ? 'es_ES' : 'en_EN'
+                        userModel.findOneAndUpdate(
+                            {
+                                USERID: user.id
+                            },
+                            {
+                                USERID: user.id,
+                                COMMANDS_EXECUTED: parseInt(user.COMMANDS_EXECUTED),
+                                VOTED: user.VOTED,
+                                BANNED: user.BANNED,
+                                LANG: lang,
+                                DEV: user.DEV,
+                                PREMIUM_COMMANDS: user.PREMIUM_COMMANDS,
+                                isINDB: user.isINDB,
+                                Roles: user.ROLES,
+                                OLDMODE: user.OLDMODE
+                            },
+                            { upsert: true }
+                        )
+                        user.LANG = lang
+                        const embed = new MessageEmbed()
+                            .setColor(process.env.EMBED_COLOR)
+                            .setTitle(client.language.SUCCESSEMBED)
+                            .setDescription(
+                                lang === 'es_ES'
+                                    ? 'Has seleccionado español como tu nuevo idioma.'
+                                    : "You've selected English as your new language"
+                            )
+                            .setFooter(message.author.username, message.author.avatarURL())
+                        return interaction.update({ embeds: [embed], components: [] })
+                    } catch (error) {
+                        console.error(error)
+                    }
+                }
+            )
         } catch (e) {
             console.error(e)
             message.channel.send({
@@ -198,22 +111,17 @@ module.exports = class Lang extends Command {
                         .setColor('RED')
                         .setTitle(client.language.ERROREMBED)
                         .setDescription(client.language.fatal_error)
-                        .setFooter(
-                            message.author.username,
-                            message.author.avatarURL()
-                        )
+                        .setFooter(message.author.username, message.author.avatarURL())
                 ]
             })
             webhookClient.send(
                 `Ha habido un error en **${message.guild.name} [ID Server: ${message.guild.id}] [ID Usuario: ${message.author.id}] [Owner: ${message.guild.ownerId}]**. Numero de usuarios: **${message.guild.memberCount}**\nMensaje: ${message.content}\n\nError: ${e}\n\n**------------------------------------**`
             )
-            try {
-                message.author
-                    .send(
-                        'Oops... Ha ocurrido un eror con el comando ejecutado. Aunque ya he notificado a mis desarrolladores del problema, ¿te importaría ir a discord.gg/nodebot y dar más información?\n\nMuchísimas gracias rey <a:corazonmulticolor:836295982768586752>'
-                    )
-                    .catch(e)
-            } catch (e) {}
+            message.author
+                .send(
+                    'Oops... Ha ocurrido un eror con el comando ejecutado. Aunque ya he notificado a mis desarrolladores del problema, ¿te importaría ir a discord.gg/nodebot y dar más información?\n\nMuchísimas gracias rey <a:corazonmulticolor:836295982768586752>'
+                )
+                .catch((e) => null)
         }
     }
 }
--- commands/Musica/search.js
+++ commands/Musica/search.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require('discord.js')
 const Command = require('../../structures/Commandos.js')
-const { DiscordMenus, ButtonBuilder, MenuBuilder } = require('discord-menus')
+const { SelectMenuBuilder, ActionRowBuilder } = require('@discordjs/builders')
+
 module.exports = class Search extends Command {
     constructor(client) {
         super(client, {
@@ -25,13 +26,8 @@ module.exports = class Search extends Command {
                 const errorembed = new MessageEmbed()
                     .setColor('RED')
                     .setTitle(client.language.ERROREMBED)
-                    .setDescription(
-                        'Necesitas estar en un canal de voz para ejecutar este comando.'
-                    )
-                    .setFooter(
-                        message.author.username,
-                        message.author.avatarURL()
-                    )
+                    .setDescription('Necesitas estar en un canal de voz para ejecutar este comando.')
+                    .setFooter(message.author.username, message.author.avatarURL())
                 return message.channel.send({ embeds: [errorembed] })
             }
             const player = client.manager.create({
@@ -52,9 +48,7 @@ module.exports = class Search extends Command {
                 const errorembed = new MessageEmbed()
                     .setColor('RED')
                     .setTitle('Error!')
-                    .setDescription(
-                        'Necesitas estar en el mismo canal de voz que el bot para ejecutar este comando.'
-                    )
+                    .setDescription('Necesitas estar en el mismo canal de voz que el bot para ejecutar este comando.')
                 return message.channel.send({ embeds: [errorembed] })
             }
             const res = await client.manager.search(sc, message.author)
@@ -62,24 +56,24 @@ module.exports = class Search extends Command {
             const tracks = res.tracks.slice(0, 5)
             const results = res.tracks
                 .slice(0, 5)
-                .map(result => `**${++n} -** [${result.title}](${result.uri})`)
+                .map((result) => `**${++n} -** [${result.title}](${result.uri})`)
                 .join('\n')
-            const res1 = res.tracks[0].title.toString()
-            const MenusManager = new DiscordMenus(client)
-            const myCoolMenu = new MenuBuilder()
-                .addLabel('res1', { description: `Prueba`, value: 'cancion-1' })
-                .addLabel('res1', { description: `Prueba`, value: 'cancion-2' })
-                .addLabel(res.tracks[0].title, {
-                    description: `Prueba`,
-                    value: 'cancion-3'
+            const myCoolMenu = new SelectMenuBuilder()
+                .setCustomId('cool-custom-id')
+                .setPlaceholder('Select an option')
+                .setMaxValues(1)
+                .setMinValues(1)
+                .setOptions(
+                    { label: 'res1', description: 'Prueba', value: 'cancion-1' },
+                    { label: 'res1', description: 'Prueba', value: 'cancion-2' },
+                    { label: res.tracks[0].title, description: 'Prueba', value: 'cancion-3' }
+                )
+            message
+                .reply({
+                    embeds: [new MessageEmbed().setDescription('Hello world!')],
+                    components: [new ActionRowBuilder().addComponents(myCoolMenu)]
                 })
-                .setCustomID('cool-custom-id')
-                .setPlaceHolder('Select an option')
-            await MenusManager.sendMenu(
-                message,
-                new MessageEmbed().setDescription('Hello world!'),
-                { menu: myCoolMenu }
-            ).catch(err => console.error(err))
+                .catch((err) => console.error(err))
         } catch (e) {
             console.error(e)
             message.channel.send({
@@ -88,22 +82,17 @@ module.exports = class Search extends Command {
                         .setColor('RED')
                         .setTitle(client.language.ERROREMBED)
                         .setDescription(client.language.fatal_error)
-                        .setFooter(
-                            message.author.username,
-                            message.author.avatarURL()
-                        )
+                        .setFooter(message.author.username, message.author.avatarURL())
                 ]
             })
             webhookClient.send(
                 `Ha habido un error en **${message.guild.name} [ID Server: ${message.guild.id}] [ID Usuario: ${message.author.id}] [Owner: ${message.guild.ownerId}]**. Numero de usuarios: **${message.guild.memberCount}**\nMensaje: ${message.content}\n\nError: ${e}\n\n**------------------------------------**`
             )
-            try {
-                message.author
-                    .send(
-                        'Oops... Ha ocurrido un eror con el comando ejecutado. Aunque ya he notificado a mis desarrolladores del problema, ¿te importaría ir a discord.gg/nodebot y dar más información?\n\nMuchísimas gracias rey <a:corazonmulticolor:836295982768586752>'
-                    )
-                    .catch(e)
-            } catch (e) {}
+            message.author
+                .send(
+                    'Oops... Ha ocurrido un eror con el comando ejecutado. Aunque ya he notificado a mis desarrolladores del problema, ¿te importaría ir a discord.gg/nodebot y dar más información?\n\nMuchísimas gracias rey <a:corazonmulticolor:836295982768586752>'
+                )
+                .catch((e) => null)
         }
     }
 }
